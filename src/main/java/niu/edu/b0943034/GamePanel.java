6304898d1f23e46f8b1185e47f2ae58b75b4e6da
Peilun <peilun2016@gmail.com>
2023-06-16 21:55:51 +0800

    完整版 v.3

--- src/main/java/niu/edu/b0943034/GamePanel.java
+++ src/main/java/niu/edu/b0943034/GamePanel.java
@@ -18,33 +18,33 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    private ExecutorService exec;
-    private Socket clientSocket;
-    private DataInputStream fromServer;
-    private DataOutputStream toServer;
+    private ExecutorService exec; // 執行緒池，用於執行資料接收任務
+    private Socket clientSocket; // 客戶端 Socket
+    private DataInputStream fromServer; // 從伺服器接收資料的輸入串流
+    private DataOutputStream toServer; // 向伺服器發送資料的輸出串流
 
-    private static final int SCREEN_WIDTH = 500;
-    private static final int SCREEN_HEIGHT = 500;
-    private static final int UNIT_SIZE = 10;
-    private static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    private static final int DELAY = 55;
+    private static final int SCREEN_WIDTH = 500; // 螢幕寬度
+    private static final int SCREEN_HEIGHT = 500; // 螢幕高度
+    private static final int UNIT_SIZE = 10; // 單位大小
+    private static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE; // 遊戲單位數量
+    private static final int DELAY = 55; // 遊戲更新間隔時間
 
-    private final int[] x = new int[GAME_UNITS];
-    private final int[] y = new int[GAME_UNITS];
-    private int bodyParts = 3;
-    private int applesEaten = 0;
-    private String receivedMessage;
-    private List<Player> playerList = new ArrayList<>();
-    private int appleX;
-    private int appleY;
-    private char direction = 'R';
-    private boolean running = false;
-    private boolean started = false;
-    private boolean paused = false;
-    private boolean gameOver = false;
-    private Timer timer = new Timer(DELAY, this);
-    private Random random;
-    private Player player;
+    private final int[] x = new int[GAME_UNITS]; // 蛇身體的 x 坐標
+    private final int[] y = new int[GAME_UNITS]; // 蛇身體的 y 坐標
+    private int bodyParts = 3; // 蛇身體的部分數量
+    private int applesEaten = 0; // 吃到的蘋果數量
+    private String receivedMessage; // 從伺服器接收的訊息
+    private List<Player> playerList = new ArrayList<>(); // 玩家列表
+    private int appleX; // 蘋果的 x 坐標
+    private int appleY; // 蘋果的 y 坐標
+    private char direction = 'R'; // 蛇的移動方向，初始為右方
+    private boolean running = false; // 遊戲是否運行中
+    private boolean started = false; // 遊戲是否已開始
+    private boolean paused = false; // 遊戲是否暫停中
+    private boolean gameOver = false; // 遊戲是否結束
+    private Timer timer = new Timer(DELAY, this); // 遊戲更新計時器
+    private Random random; // 隨機數生成器
+    private Player player; // 玩家資訊
 
     public GamePanel() {
         random = new Random();
@@ -118,9 +118,9 @@ public class GamePanel extends JPanel implements ActionListener {
         if (started && paused) {
             return;
         }
-        updateScore(player, 0);
-        newApple();
-        running = true;
+        updateScore(player, 0); // 更新玩家分數為 0
+        newApple(); // 產生新的蘋果
+        running = true; // 開始遊戲
 
         // 計算螢幕中心點的坐標
         int centerX = SCREEN_WIDTH / 2;
@@ -136,7 +136,7 @@ public class GamePanel extends JPanel implements ActionListener {
             y[i] = centerY;
         }
 
-        timer.start();
+        timer.start(); // 啟動遊戲更新計時器
     }
 
     // 產生新的蘋果
@@ -148,11 +148,11 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent arg0) {
         if (running && !paused) {
-            move();
-            checkApple();
-            checkCollisions();
+            move(); // 移動蛇
+            checkApple(); // 檢查是否吃到蘋果
+            checkCollisions(); // 檢查碰撞
         }
-        repaint();
+        repaint(); // 重新繪製遊戲畫面
     }
 
     // 鍵盤事件監聽器
@@ -162,42 +162,42 @@ public class GamePanel extends JPanel implements ActionListener {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_LEFT:
                     if (direction != 'R') {
-                        direction = 'L';
+                        direction = 'L'; // 左
                     }
                     break;
 
                 case KeyEvent.VK_RIGHT:
                     if (direction != 'L') {
-                        direction = 'R';
+                        direction = 'R'; // 右
                     }
                     break;
 
                 case KeyEvent.VK_UP:
                     if (direction != 'D') {
-                        direction = 'U';
+                        direction = 'U'; // 上
                     }
                     break;
 
                 case KeyEvent.VK_DOWN:
                     if (direction != 'U') {
-                        direction = 'D';
+                        direction = 'D'; // 下
                     }
                     break;
 
                 case KeyEvent.VK_SPACE:
                     if (paused) {
-                        resume();
+                        resume(); // 恢復遊戲
                     } else if (running) {
-                        pause();
+                        pause(); // 暫停遊戲
                     }
                     break;
                 case KeyEvent.VK_ENTER:
                     if (!started) {
                         started = true;
-                        startGame();
+                        startGame(); // 開始遊戲
                     } else if (gameOver) {
                         started = false;
-                        restartGame();
+                        restartGame(); // 重新開始遊戲
                     }
                     break;
             }
@@ -212,16 +212,16 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         switch (direction) {
             case 'U':
-                y[0] -= UNIT_SIZE;
+                y[0] -= UNIT_SIZE; // 向上移動
                 break;
             case 'D':
-                y[0] += UNIT_SIZE;
+                y[0] += UNIT_SIZE; // 向下移動
                 break;
             case 'L':
-                x[0] -= UNIT_SIZE;
+                x[0] -= UNIT_SIZE; // 向左移動
                 break;
             case 'R':
-                x[0] += UNIT_SIZE;
+                x[0] += UNIT_SIZE; // 向右移動
                 break;
         }
     }
@@ -229,10 +229,10 @@ public class GamePanel extends JPanel implements ActionListener {
     // 檢查是否吃到蘋果
     public void checkApple() {
         if ((x[0] == appleX) && (y[0] == appleY)) {
-            bodyParts++;
-            applesEaten++;
-            updateScore(player, applesEaten);
-            newApple();
+            bodyParts++; // 蛇身體增加一部分
+            applesEaten++; // 吃到的蘋果數量增加
+            updateScore(player, applesEaten); // 更新玩家分數
+            newApple(); // 產生新的蘋果
         }
     }
 
@@ -240,22 +240,22 @@ public class GamePanel extends JPanel implements ActionListener {
     public void checkCollisions() {
         for (int i = bodyParts; i > 0; i--) {
             if ((x[0] == x[i]) && (y[0] == y[i])) {
-                running = false;
+                running = false; // 蛇頭碰到自己的身體，遊戲結束
             }
         }
         if (x[0] < 0 || x[0] > SCREEN_WIDTH || y[0] < 0 || y[0] > SCREEN_HEIGHT) {
-            running = false;
+            running = false; // 蛇頭碰到邊界，遊戲結束
         }
         if (!running) {
-            timer.stop();
-            paused = false;
+            timer.stop(); // 停止遊戲更新計時器
+            paused = false; // 取消暫停狀態
         }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        draw(g);
+        draw(g); // 繪製遊戲畫面
     }
 
     // 更新伺服器資料
@@ -263,16 +263,16 @@ public class GamePanel extends JPanel implements ActionListener {
         Gson gson = new Gson();
         Type type = new TypeToken<List<Player>>() {
         }.getType();
-        playerList = gson.fromJson(receivedMessage, type);
+        playerList = gson.fromJson(receivedMessage, type); // 將接收到的 JSON 資料轉換為玩家列表
     }
 
     // 傳送玩家資料至伺服器
     public void sendMessage(Player player) {
         try {
             Gson gson = new Gson();
-            String json = gson.toJson(player);
+            String json = gson.toJson(player); // 將玩家物件轉換為 JSON 字串
             System.out.println("Send: " + json);
-            toServer.writeUTF(json);
+            toServer.writeUTF(json); // 發送 JSON 字串至伺服器
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -280,24 +280,24 @@ public class GamePanel extends JPanel implements ActionListener {
 
     // 更新玩家分數
     public void updateScore(Player player, int score) {
-        player.setScore(score);
-        player.setBest(score);
-        sendMessage(player);
+        player.setScore(score); // 更新玩家分數
+        player.setBest(score); // 更新玩家最高分數
+        sendMessage(player); // 傳送玩家資料至伺服器
     }
 
     // 繪製遊戲畫面
     public void draw(Graphics g) {
         if (!started) {
-            drawStartScreen(g);
+            drawStartScreen(g); // 繪製開始畫面
         } else if (running) {
-            drawSnake(g);
-            drawPlayerList(g);
-            drawTopRank(g);
+            drawSnake(g); // 繪製蛇
+            drawPlayerList(g); // 繪製玩家列表
+            drawTopRank(g); // 繪製排行榜
             if (paused) {
-                drawPauseScreen(g);
+                drawPauseScreen(g); // 繪製暫停畫面
             }
         } else {
-            gameOver(g);
+            gameOver(g); // 繪製遊戲結束畫面
         }
     }
 
@@ -316,11 +316,11 @@ public class GamePanel extends JPanel implements ActionListener {
     // 繪製蛇
     private void drawSnake(Graphics g) {
         g.setColor(Color.RED);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); // 繪製蘋果
 
         for (int i = 0; i < bodyParts; i++) {
             g.setColor(i == 0 ? Color.BLUE : Color.GREEN);
-            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE); // 繪製蛇的身體部分，蛇頭為藍色，其餘部分為綠色
         }
     }
 
@@ -335,7 +335,7 @@ public class GamePanel extends JPanel implements ActionListener {
         text_y++;
 
         Comparator<Player> scoreComparator = (p1, p2) -> p2.getScore() - p1.getScore();
-        Collections.sort(playerList, scoreComparator);
+        Collections.sort(playerList, scoreComparator); // 根據分數排序玩家列表
 
         for (Player p : playerList) {
             if (p.getOnline()) {
@@ -360,7 +360,7 @@ public class GamePanel extends JPanel implements ActionListener {
         text_y++;
 
         Comparator<Player> scoreComparator = (p1, p2) -> p2.getBest() - p1.getBest();
-        List<Player> topPlayers = playerList.stream().sorted(scoreComparator).limit(3).collect(Collectors.toList());
+        List<Player> topPlayers = playerList.stream().sorted(scoreComparator).limit(3).collect(Collectors.toList()); // 根據最高分數排序取前三名
 
         for (Player p : topPlayers) {
             g.setFont(new Font("Monospaced", Font.BOLD, 14));
@@ -379,6 +379,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     // 繪製遊戲結束畫面
     public void gameOver(Graphics g) {
+
         Font font = new Font("Monospaced", Font.BOLD, 36);
         drawCenteredString(g, font, Color.RED, "遊戲結束", SCREEN_HEIGHT / 2);
 
--- src/main/java/niu/edu/b0943034/GameServer.java
+++ src/main/java/niu/edu/b0943034/GameServer.java
@@ -16,34 +16,53 @@ import javax.swing.*;
 
 public class GameServer extends JFrame {
 
+    // 日期時間格式化物件
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    // 顯示伺服器運行狀態的文字區域
     private JTextArea txt = new JTextArea("[" + sdf.format(new Date()) + "] " + "伺服器運行中...\n");
+
+    // 伺服器Socket物件
     private ServerSocket serverSocket = null;
+
+    // 存儲客戶端Socket的Map
     private static Map<Integer, Socket> socketMap = new HashMap<>();
+
+    // 存儲玩家清單的List
     private static List<Player> playerList = new ArrayList<>();
 
+    // 執行緒池
     private ExecutorService exec = null;
 
     public GameServer(int port) throws IOException, SocketException {
         setTitle("Snake Server");
 
+        // 設置文字區域為不可編輯並添加到視窗中
         txt.setEditable(false);
         txt.append("[" + sdf.format(new Date()) + "] " + "執行於 port:" + port + "...\n");
-
         setLayout(new BorderLayout());
         this.add(new JScrollPane(txt), BorderLayout.CENTER);
         setSize(500, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
+        // 創建ServerSocket並監聽指定的port
         serverSocket = new ServerSocket(port);
+
+        // 創建執行緒池
         exec = Executors.newCachedThreadPool();
 
+        // 載入玩家清單
         loadPlayerList();
-        initOnlineStatue();
+
+        // 初始化所有玩家的線上狀態
+        initOnlineStatus();
 
         while (true) {
+            // 接受客戶端的連線
             Socket socket = serverSocket.accept();
+
+            // 創建通訊執行緒並加入執行緒池中
             exec.execute(new Communication(socket, playerList, txt, sdf, socketMap));
         }
     }
@@ -51,10 +70,12 @@ public class GameServer extends JFrame {
     // 載入玩家清單
     public void loadPlayerList() {
         try {
+            // 檢查玩家清單檔案是否存在，若不存在則創建新的檔案
             File file = new File("playerList.json");
             if (!file.exists()) {
                 file.createNewFile();
             } else {
+                // 讀取玩家清單檔案中的內容並轉換為List<Player>物件
                 BufferedReader br = new BufferedReader(new FileReader(file));
                 StringBuilder sb = new StringBuilder();
                 String line;
@@ -75,6 +96,7 @@ public class GameServer extends JFrame {
     // 儲存玩家清單
     public static void savePlayerList() {
         try {
+            // 將玩家清單轉換為JSON格式並寫入檔案中
             Gson gson = new Gson();
             String json = gson.toJson(playerList);
             FileWriter writer = new FileWriter("playerList.json");
@@ -88,6 +110,7 @@ public class GameServer extends JFrame {
     // 傳送玩家清單給所有連線的客戶端
     public static void sendPlayerList(Map<Integer, Socket> socketMap) {
         try {
+            // 將玩家清單轉換為JSON格式並傳送給每個客戶端
             Gson gson = new Gson();
             String json = gson.toJson(playerList);
             System.out.println("Send: " + json);
@@ -103,28 +126,34 @@ public class GameServer extends JFrame {
 
     // 更新玩家清單
     public static void updatePlayerList(Player player) {
+        // 檢查玩家是否已存在於清單中
         Player existingPlayer = playerList.stream()
                 .filter(p -> p.getName().equals(player.getName()) && p.getPort() == player.getPort())
                 .findFirst()
                 .orElse(null);
         if (existingPlayer != null) {
+            // 若玩家已存在，則更新分數和最佳分數，並將線上狀態設為true
             existingPlayer.setScore(player.getScore());
             if (existingPlayer.getBest() < player.getBest()) {
                 existingPlayer.setBest(player.getBest());
             }
             existingPlayer.setOnline(true);
         } else {
+            // 若玩家不存在，則將玩家添加到清單中
             playerList.add(player);
         }
+        // 儲存玩家清單並傳送給所有連線的客戶端
         savePlayerList();
         sendPlayerList(socketMap);
     }
 
     // 初始化所有玩家的線上狀態
-    public void initOnlineStatue() {
+    public void initOnlineStatus() {
+        // 將所有玩家的線上狀態設為false
         for (Player player : playerList) {
             player.setOnline(false);
         }
+        // 儲存玩家清單並傳送給所有連線的客戶端
         savePlayerList();
         sendPlayerList(socketMap);
     }
@@ -146,9 +175,15 @@ public class GameServer extends JFrame {
             this.txt = txt;
             this.sdf = sdf;
             this.socketMap = socketMap;
+
+            // 創建從客戶端讀取訊息的DataInputStream物件
             fromClient = new DataInputStream(socket.getInputStream());
+
+            // 將客戶端Socket加入socketMap中並傳送玩家清單給該客戶端
             socketMap.put(socket.getPort(), socket);
             sendPlayerList(socketMap);
+
+            // 在文字區域中顯示連線成功的訊息
             txt.append("[" + sdf.format(new Date()) + "]" + "[ :" + socket.getPort() + "] 連接成功\n");
         }
 
@@ -156,20 +191,28 @@ public class GameServer extends JFrame {
         public void run() {
             try {
                 while ((msg = fromClient.readUTF()) != null) {
+                    // 接收客戶端傳送的訊息並解析為Player物件
                     System.out.println("Received: " + msg);
                     Gson gson = new Gson();
                     Player player = gson.fromJson(msg, Player.class);
                     player.setOnline(false);
+
+                    // 更新玩家清單並在文字區域中顯示分數更新訊息
                     updatePlayerList(player);
                     txt.append("[" + sdf.format(new Date()) + "]" + "[" + player.getName() + ":" + socket.getPort() + "] 更新分數為 "
                             + player.getScore() + "\n");
                 }
             } catch (Exception e) {
+                // 在文字區域中顯示斷開連線的訊息
                 txt.append("[" + sdf.format(new Date()) + "]" + "[ :" + socket.getPort() + "] 斷開連線\n");
+
+                // 獲取斷開連線的玩家並設置線上狀態為false，從socketMap中移除該客戶端Socket
                 Player disconnectedPlayer = playerList.stream().filter(p -> p.getPort() == socket.getPort()).findFirst().orElse(null);
                 if (disconnectedPlayer != null) {
                     disconnectedPlayer.setOnline(false);
                     socketMap.remove(socket.getPort());
+
+                    // 儲存玩家清單並傳送給所有連線的客戶端
                     savePlayerList();
                     sendPlayerList(socketMap);
                 }
